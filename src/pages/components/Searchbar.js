f0cd2bafa78a53ad7a68e411f429aa4c0097e247
KennardHans <kennard112356@gmail.com>
2022-01-18 12:48:29 +0800

    limited options to 3

--- src/pages/components/Searchbar.js
+++ src/pages/components/Searchbar.js
@@ -1,59 +1,74 @@
 import { AutoComplete } from "antd";
-import '../components/Searchbar.css';
-import 'antd/dist/antd.css';
-import { useEffect, useState } from 'react';
+import "../components/Searchbar.css";
+import "antd/dist/antd.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
 const Searchbar = () => {
-    let [input,setInput] = useState('');
-    let handleChange = (e) =>{
-        setInput(e);
+  let [input, setInput] = useState("");
+  let handleChange = (e) => {
+    setInput(e);
+  };
+  let [result, setResult] = useState("");
+  let [next, setNext] = useState("");
+  let option = [];
+  let arr = [];
+  useEffect(() => {
+    let call = async () => {
+      await axios.get("https://swapi.dev/api/people/").then((res) => {
+        setResult(res.data.results);
+        setNext(res.data.next);
+      });
+    };
+    call();
+  }, []);
+  useEffect(() => {
+    arr = [];
+    let i = 0;
+    let j = 0;
+    while (i < result.length) {
+      if (result[i].name.toUpperCase().indexOf(input.toUpperCase()) !== -1) {
+        arr.push(result[i].name);
+      }
+      i++;
     }
-    let [result,setResult] = useState('');
-    let [next,setNext] = useState('');
-    let arr = [];
-    useEffect(() => {
-        let call = async () => {
-            await axios.get('https://swapi.dev/api/people/').then(res =>{
-                setResult(res.data.results);
-                setNext(res.data.next);
-            })
+    console.log(arr);
+    while(option.length<3){
+        if(arr[j]!== undefined){
+            option.push(
+                {
+                    value: arr[j]
+                }
+            )
         }
-        call();
-    },[])
-    useEffect(() => {
-        arr = [];
-        let i = 0;
-        while(i<result.length){
-            if(result[i].name.toUpperCase().indexOf(input.toUpperCase()) !== -1){
-                arr.push(result[i].name)
-            }
-            i++;
+        else{
+            break;
         }
-        console.log(arr)
-    },[input])
-    let option = [
-        {
-            value:arr[0]
-        }
-    ]
-    return ( 
-        <div>
-            <div>
-            <AutoComplete
-                className="autocomplete"
-                placeholder="your character name"
-                onChange={handleChange}
-                options={option}
-                filterOption={(inputValue,option) =>{
-                    return option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-                }}
-            />
-            </div>
-            <div><button className="sign_in">SIGN IN</button></div>
+        j++;
+    }
+    console.log(option)
+  }, [input]);
 
-        </div>
-       
-     );
-}
- 
+  return (
+    <div>
+      <div>
+        <AutoComplete
+          className="autocomplete"
+          placeholder="your character name"
+          onChange={handleChange}
+          options={option}
+          filterOption={(inputValue, option) => {
+            return (
+              option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
+              -1
+            );
+          }}
+        />
+      </div>
+      <div>
+        <button className="sign_in">SIGN IN</button>
+      </div>
+    </div>
+  );
+};
+
 export default Searchbar;
