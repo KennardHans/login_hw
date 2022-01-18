23652909c4dfba168198883ab9213bf0e57cf68e
KennardHans <kennard112356@gmail.com>
2022-01-18 19:59:28 +0800

    fixed search lag

--- src/pages/components/Searchbar.js
+++ src/pages/components/Searchbar.js
@@ -4,14 +4,48 @@ import "antd/dist/antd.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const Searchbar = () => {
-  let [input, setInput] = useState("");
-  let [result, setResult] = useState("");
-  let [next, setNext] = useState("");
-  let [options,setOptions] = useState([]);
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const [options,setOptions] = useState([]);
   let option = []
   let arr = [];
   let handleChange = (e) => {
     setInput(e);
+    // arr = [];
+    // let i = 0;
+    // let j = 0;
+    // while (i < result.length) {
+    //   if (result[i].name.toUpperCase().indexOf(input.toUpperCase()) !== -1) {
+    //     arr.push(result[i].name);
+    //   }
+    //   i++;
+    // }
+    // console.log(arr);
+    // while(option.length<3){
+    //     if(arr[j]!== undefined){
+    //         option.push(
+    //             {
+    //                 value: arr[j]
+    //             }
+    //         )
+    //     }
+    //     else{
+    //         break;
+    //     }
+    //     setOptions(option)
+    //     j++;
+    // }
+    // console.log(options)
+  };
+  useEffect(() => {
+    let call = async () => {
+      await axios.get("https://swapi.dev/api/people/").then((res) => {
+        setResult(res.data.results);
+      });
+    };
+    call();
+  }, []);
+  useEffect(() => {
     arr = [];
     let i = 0;
     let j = 0;
@@ -37,41 +71,6 @@ const Searchbar = () => {
         j++;
     }
     console.log(options)
-  };
-  useEffect(() => {
-    let call = async () => {
-      await axios.get("https://swapi.dev/api/people/").then((res) => {
-        setResult(res.data.results);
-        setNext(res.data.next);
-      });
-    };
-    call();
-  }, []);
-  useEffect(() => {
-    // arr = [];
-    // let i = 0;
-    // let j = 0;
-    // while (i < result.length) {
-    //   if (result[i].name.toUpperCase().indexOf(input.toUpperCase()) !== -1) {
-    //     arr.push(result[i].name);
-    //   }
-    //   i++;
-    // }
-    // console.log(arr);
-    // while(option.length<3){
-    //     if(arr[j]!== undefined){
-    //         option.push(
-    //             {
-    //                 value: arr[j]
-    //             }
-    //         )
-    //     }
-    //     else{
-    //         break;
-    //     }
-    //     j++;
-    // }
-    // console.log(option)
   }, [input]);
 
   return (
@@ -82,12 +81,12 @@ const Searchbar = () => {
           placeholder="your character name"
           onChange={handleChange}
           options={options}
-        //   filterOption={(inputValue, option) => {
-        //     return (
-        //       option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
-        //       -1
-        //     );
-        //   }}
+            // filterOption={(inputValue, option) => {
+            //     return (
+            //     option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
+            //     -1
+            //     );
+            // }}
         />
       </div>
       <div>
